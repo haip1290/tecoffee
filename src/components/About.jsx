94066a62e785ec5c7f60cca0eacc9d75601951f6
Hai Pham <thanh_hai1290@yahoo.com>
2026-04-27 13:07:14 +0700

    feat: add aboutus section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
-import manifestoImg from "../assets/products/dirtymatcha.jpg";
+import manifestoImg from "../assets/interior/working_customr.jpg";
 const About = () => {
   return (
     <section className="flex items-center max-h-screen md:h-[80vh] overflow-hidden bg-stone-50">
-      <div className="flex-1 flex flex-col justify-center items-center px-10 md:px-20 lg:px-32 text-center space-y-8">
+      <div className="flex-4 flex flex-col justify-center items-center px-10 md:px-20 lg:px-32 text-center space-y-8">
         {/* header */}
         <span className="text-stone-400 uppercase tracking-[0.3em] text-xs">
           Our Manifesto
@@ -11,7 +11,7 @@ const About = () => {
         <h2 className="text-3xl md:text-4xl font-light text-stone-800 leading-tight">
           A quiet corner for
           <span className="italic font-serif text-sm">small talks</span> and
-          <span className="font-serif text-5xl"> big ideas.</span>
+          <span className="font-serif text-6xl"> big ideas.</span>
         </h2>
 
         {/* manifesto */}
@@ -30,10 +30,10 @@ const About = () => {
         </div>
       </div>
 
-      <div className="flex-1 h-full rounded-2xl">
+      <div className="flex justify-center flex-3 h-full p-8">
         <img
           src={manifestoImg}
-          className="max-w-full max-h-full object-scale-down"
+          className="max-h-full object-scale-down rounded-2xl"
           alt="signature drink on table"
           srcset=""
         />
